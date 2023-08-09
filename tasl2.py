cf0ef2c28d449c70374cd78a0f2745fff90ce555
VarentsovaMV <varentsovamv@gmail.com>
2023-08-09 16:49:17 +0300

    test

---
+++ tasl2.py
@@ -0,0 +1,16 @@
+# a = 3
+# b = 5
+# c = 10
+#
+# # Введите ваше решение ниже
+# if a * b > c and (c % a == 0 or c % b == 0):
+#     print('yes')
+# else:
+#     print('no')\ \
+n = 3
+m = 5
+k = 10
+if k <= m * n and (k % m == 0 or k % n == 0):
+    print('YES')
+else:
+    print('NO')
